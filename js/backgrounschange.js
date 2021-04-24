85d8979559183ed43182b0a016680832c17fa388
Irvin Buendia <irvin.buendia1@gmail.com>
2021-04-24 15:17:30 -0500

    started working on iselas page

---
+++ js/backgrounschange.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+<script>
+    if (b.className="yes")
+    {
+        document.getElementById("img").src="Site/assets/HappyObama.jpg";
+    }
+    else
+    {
+        document.getElementById("img").src="Site/assets/SadObama.jpg";
+    }
+</script>
