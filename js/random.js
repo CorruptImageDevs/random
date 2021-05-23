63ba4570b5f2a7ff74c6631b34966168ab6a38f9
Irvin Buendia <irvin.buendia1@gmail.com>
2021-05-22 22:50:15 -0500

    random

--- js/random.js
+++ js/random.js
@@ -142,3 +142,8 @@
 //     });
 // });
 
+const paragraphs = document.querySelectorAll("p");
+// paragraphs[0] is the first <p> element
+// paragraphs[1] is the second <p> element, etc.
+alert(paragraphs[0].nodeName);
+
